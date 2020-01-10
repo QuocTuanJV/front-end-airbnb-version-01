1790188cb79390b9c7a4e1839316caa47dc6408c
QuocTuanJV <quoctuan.naf@gmail.com>
2020-01-10 10:34:28 +0700

    create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SignUpComponent } from './component/sign-up/sign-up.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
