95b1c1e83c2567c051f000db48cb97099706318d
QuocTuanJV <quoctuan.naf@gmail.com>
2020-01-15 15:18:02 +0700

    add path and registerComponent to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent
   }
 ];
 @NgModule({
