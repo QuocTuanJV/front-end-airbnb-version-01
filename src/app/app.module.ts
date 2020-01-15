df598a38bb7e95efe43c44ec90a69cf6530fb793
QuocTuanJV <quoctuan.naf@gmail.com>
2020-01-15 16:49:48 +0700

    add FormModule to create object from form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MenuComponent } from './component/menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SignUpComponent } from './component/sign-up/sign-up.component';
 import { HomePageComponent } from './component/home-page/home-page.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomePageComponent } from './component/home-page/home-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
