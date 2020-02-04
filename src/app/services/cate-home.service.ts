83dd4d4096b449cda687f7be063640d360d45a21
QuocTuanJV <quoctuan.naf@gmail.com>
2020-02-04 17:07:59 +0700

    cate-home.service.ts: add new category Home request to server

--- src/app/services/cate-home.service.ts
+++ src/app/services/cate-home.service.ts
@@ -7,10 +7,15 @@ import {Observable} from 'rxjs';
 })
 export class CateHomeService {
   private urlGetCateHome = 'http://localhost:8080/api/auth/cate-homes';
+  private urlAddCateHome = 'http://localhost:8080/api/auth/add-cate-home'
 
   constructor(private http: HttpClient) { }
 
   getCateHome(): Observable<CateHome[]> {
     return this.http.get<CateHome[]>(this.urlGetCateHome);
+  }
+
+  addNewCateHome(cateHomeRegisInfo: CateHome): Observable<any> {
+    return this.http.post<any>(this.urlAddCateHome, cateHomeRegisInfo);
   }
 }
