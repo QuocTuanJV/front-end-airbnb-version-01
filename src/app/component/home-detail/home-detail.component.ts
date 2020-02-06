308ccef36113a4e42d0849d6768733b824028f43
QuocTuanJV <quoctuan.naf@gmail.com>
2020-02-06 10:11:00 +0700

    get data to display, include: name category room, home, name category hom

--- src/app/component/home-detail/home-detail.component.ts
+++ src/app/component/home-detail/home-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HomeService} from '../../services/home.service';
+import {ActivatedRoute} from '@angular/router';
+import {CateRoomService} from '../../cate-room.service';
+import {CateHomeService} from '../../services/cate-home.service';
 
 @Component({
   selector: 'app-home-detail',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-detail.component.css']
 })
 export class HomeDetailComponent implements OnInit {
+  home: Home = {};
+  cateRoom: CateRoom = {};
+  cateHome: CateHome = {};
 
-  constructor() { }
+  constructor(private homeService: HomeService,
+              private cateRoomService: CateRoomService,
+              private cateHomeService: CateHomeService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getHomeById();
+  }
+
+   getHomeById() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.homeService.getHome(id).subscribe(data => {
+      this.home = data;
+      this.setNameCateHomeOfHome(this.home.cateHome);
+      this.setNameCateRoomOfHome(this.home.cateRoom);
+    });
+  }
+  setNameCateRoomOfHome(id: any) {
+    this.cateRoomService.getNameCateRoomRequest(id).subscribe(result => {
+      this.cateRoom = result;
+    });
+  }
+  setNameCateHomeOfHome(id: any) {
+    this.cateHomeService.getNameCateHomeRequest(id).subscribe(result => {
+      this.cateHome = result;
+    });
   }
 
 }
