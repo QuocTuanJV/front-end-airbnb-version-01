a121eba4db1ca7bc4fad2ccbc78590476bf53697
QuocTuanJV <quoctuan.naf@gmail.com>
2020-02-10 11:43:09 +0700

    Create object booking to save

--- src/app/component/booking-detail/booking-detail.component.ts
+++ src/app/component/booking-detail/booking-detail.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ActivatedRoute, NavigationStart, Router} from '@angular/router';
 import {filter, map} from 'rxjs/operators';
+import {TokenStorageService} from '../../services/token-storage.service';
+import {BookingService} from '../../services/booking.service';
 
 @Component({
   selector: 'app-booking-detail',
@@ -11,19 +13,59 @@ import {filter, map} from 'rxjs/operators';
 export class BookingDetailComponent implements OnInit {
   state$: Observable<Home>;
   homeBooking: Home = {};
-  constructor(private activatedRoute: ActivatedRoute) { }
+  bookingInfo: Booking = {};
+  userNameBooking = '';
+  idUserBooking = 0;
+  form: any = {};
+  constructor(private activatedRoute: ActivatedRoute,
+              private tokenStorageService: TokenStorageService,
+              private bookingService: BookingService,
+              private router: Router) { }
 
   ngOnInit() {
-    this.getHomeBooking();
+    this.getUserNameBooking();
+    console.log('Init: ' + this.userNameBooking);
+    this.setIdUserBooking(this.userNameBooking);
+    console.log('Init: ' + this.idUserBooking);
   }
 
   getHomeBooking() {
     this.state$ = this.activatedRoute.paramMap.pipe(map(() => window.history.state));
     this.state$.subscribe(result => {
       this.homeBooking = result;
+      this.bookingInfo = {
+        idUser: this.idUserBooking,
+        idHome: this.homeBooking.id,
+        timeBook: this.form.timeBook,
+        timeBookOut: this.form.timeBookOut
+      };
     });
   }
 
   onSubmit() {
+    this.getHomeBooking();
+    console.log('id User booking: ' + this.bookingInfo.idUser);
+    console.log('id Home booking: ' + this.bookingInfo.idHome);
+    console.log('Time booking: ' + this.bookingInfo.timeBook);
+    this.createBooking(this.bookingInfo);
+  }
+
+  setIdUserBooking(nameUser: any) {
+    this.bookingService.getIdUserBooking(nameUser).subscribe(result =>
+    this.idUserBooking = result);
+  }
+
+  getUserNameBooking(): void {
+    this.userNameBooking = this.tokenStorageService.getUsername();
+  }
+  createBooking(booking: Booking) {
+    this.bookingService.create(booking).subscribe(result => {
+      alert('Booking successfully!');
+      this.goHomePage();
+    });
+  }
+
+  goHomePage() {
+    this.router.navigate(['/']);
   }
 }
